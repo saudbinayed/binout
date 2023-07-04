67f4a3702f15318a897adaf1d66ffa734297f66b
saudbinayed <138231342+saudbinayed@users.noreply.github.com>
2023-07-04 04:22:52 +0100

    1

--- src/struct2graph.m
+++ src/struct2graph.m
@@ -13,7 +13,6 @@ function [figParents,figSelect]=struct2graph(binin,labelQ,iterMax)
 % ... If iterMax is omitted, it defaults to 400.
 % ===============
 %% Output:
-% figAll = handle to the figrue containing the most overall graph (the big graph). For visual reasons, this graph layout is set as "force" (similar to neural networks)
 % figParents = handle to the figure containing the most overall graph (similar to above) but without terminal leaves, i.e. only parents are shown
 % figSelect = (possibly array of) handles to figures containing the subgraphs requested by "labelQ". If labelQ=="All", then figSelect is array of all sub-graphs
 % ==============
@@ -36,10 +35,10 @@ parentLabel={''};
 done={};
 %
 %
-if nargin<2
+if nargin<2 || (nargin>=2 && isempty(labelQ))
     labelQ="All";
 end
-if nargin<3
+if nargin<3 || (nargin>=3 && isempty(iterMax))
 iterMax=400;
 end
 iter=0;
@@ -86,30 +85,6 @@ source=source(2:end); target=target(2:end);
 %
 G=graph(source,target);
 snId=unique(source);
-% figure(1); clf; cla;
-% fig=gcf;
-% set(fig,'defaultfigurewindowstyle','modal');
-% fig.Units='centimeters';
-% fig.Position=[10 10 10 10];
-% ax=gca;
-% ax.Position=[0 0 1 1];
-% pg=plot(G);
-% pg.layout("force","UseGravity","on","WeightEffect","direct","Iterations",150);
-% pg.NodeLabel(1:G.numnodes)={''};
-% snId=unique(source);
-% x=pg.XData;
-% y=pg.YData;
-% set(ax,'defaulttextInterpreter','none','defaulttextfontname','Lucida Console','defaulttextfontsize',6);
-% pg.NodeLabel(snId)=cellstr(labels(snId));
-% pg.NodeFontName='Lucida Console';
-% seId=find(ismember(G.Edges.EndNodes(:,2),snId));
-% pg.NodeColor=repmat(pg.NodeColor,G.numnodes,1);
-% pg.NodeColor(snId,:)=repmat([0.8500,0.3250,0.0980],length(snId),1);
-% pg.EdgeColor=repmat(pg.EdgeColor,G.numedges,1);
-% pg.EdgeColor(seId,:)=repmat([0.8500,0.3250,0.0980],length(seId),1);
-% figAll=fig;
-% figAll.Name="All in "+inputname(1);
-%
 %
 %
 G2=G.subgraph(snId);
@@ -131,36 +106,23 @@ snId2=unique(G2.Edges.EndNodes(:,1));
 set(ax,'defaulttextInterpreter','none','defaulttextfontname','Lucida Console','defaulttextfontsize',8);
 set(ax,'defaultaxesunits','normalized','defaultaxesposition',[0 0 1 1]);
 te2=text(x2,y2,labels2,'HorizontalAlignment','center','VerticalAlignment','middle','EdgeColor','k','LineWidth',0.08,'BackgroundColor',0.95*[1 1 1],'Margin',1);
-% te2=text(x2,y2,labels2,'HorizontalAlignment','center','VerticalAlignment','middle','EdgeColor','none','BackgroundColor','none');
-% iterMax=4;
-% for i=1:iterMax
 [y2unq,~,y2unqidx]=unique(y2);
 y2crit=y2unq(floor(mode(y2unqidx)));
 y2idx=find(y2==y2crit);
 te2Extent=cell2mat(arrayfun(@(x) x.Extent,te2,'UniformOutput',false));
 xlim([min(x2'-te2Extent(:,3)/2) max(x2'+te2Extent(:,3)/2)]);
-
 gmin=-Inf;
 iter=0;
 iterMax=20;
 while gmin<0.2 && iter<=iterMax
     iter=iter+1;
-    %fig.Position(3)=1.2*fig.Position(3);
     fig.Position(3)=fig.Position(3)+2;
     te2LW=cell2mat(arrayfun(@(x) x.Extent([1,3]),te2(y2idx),'UniformOutput',false));
     gaps2=te2LW(2:end,1)-sum(te2LW(1:end-1,:),2);
     [gmin,gidx]=min(gaps2);
 end
-% xmax2=max(max(x2),max(te2Extent(:,1)+2*te2Extent(:,3)));
-% xmin2=min(min(x2),min(te2Extent(:,1)-te2Extent(:,3)));
-% ymin2=min(min(y2),min(te2Extent(:,2)-te2Extent(:,4)/1));
-% ymax2=max(max(y2),max(te2Extent(:,2)+2*te2Extent(:,4)/1));
-% xlim([xmin2 xmax2]);
-% ylim([ymin2 ymax2]);
-% end
 figParents=fig;
 figParents.Name="Parents in "+inputname(1);
-%
 %
 %
 if length(labelQ)==1 && strcmpi(labelQ,"all")
@@ -195,26 +157,20 @@ pg3.NodeLabel={};
 pg3.set('EdgeColor','k','NodeColor','k','MarkerSize',2.5);
 xlim([min(pg3.XData)-0.5,max(pg3.XData)+3*max(strlength(labels3(snId3c)))/24]);
 ylim([min(pg3.YData)-0.5,max(pg3.YData)+0.5]);
-
 x3=pg3.XData;
 y3=pg3.YData;
 set(ax,'defaulttextInterpreter','none','defaulttextfontname','Lucida Console','defaulttextfontsize',8,'defaulttextmargin',0.9);
 set(ax,'defaultaxesunits','normalized','defaultaxesposition',[0 0 1 1]);
 te3_1=text(x3(snId3),y3(snId3),labels3(snId3),'HorizontalAlignment','center','VerticalAlignment','middle','EdgeColor','k','BackgroundColor',0.95*[1 1 1]);
 te3_2=text(x3(snId3c)+0.09,y3(snId3c),labels3(snId3c),'HorizontalAlignment','left','VerticalAlignment','middle');
-
 te3_1Extent=cell2mat({te3_1.Extent}');
 te3_2Extent=cell2mat({te3_2.Extent}');
-
 xmin=min([min(x3),min(te3_1Extent(:,1)),min(te3_2Extent(:,1))]);
 ymin=min([min(y3),min(te3_1Extent(:,2)),min(te3_2Extent(:,2))]);
-
 xmax=max([max(x3),max(sum(te3_1Extent(:,[1,3]),2)),max(sum(te3_2Extent(:,[1,3]),2))]);
 ymax=max([max(y3),max(sum(te3_1Extent(:,[2,4]),2)),max(sum(te3_2Extent(:,[2,4]),2))]);
-
 xline([xmin,xmax],'--','Color',0.99*[1 1 1]);
 yline([ymin,ymax],'--','Color',0.99*[1 1 1]);
-
 figSelect(j)=fig;
 figSelect(j).Name=labelQ(j);
 end
